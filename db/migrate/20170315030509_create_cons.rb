3d3510f559489bd6937822dab7064a3c3f2586a8
Pie <jadetong@gmail.com>
2017-03-23 00:11:48 -0700

    add years and months to nat and con's age guess

--- db/migrate/20170315030509_create_cons.rb
+++ db/migrate/20170315030509_create_cons.rb
@@ -2,16 +2,26 @@ class CreateCons < ActiveRecord::Migration[5.0]
   def change
     create_table :cons do |t|
     	t.string :name
-    	t.integer :age0
-    	t.integer :age1
-    	t.integer :age2
-    	t.integer :age3
-    	t.integer :age4
-		t.integer :age5
-		t.integer :age6
-		t.integer :age7
-		t.integer :age8
-		t.integer :age9
+      t.integer :age0y
+      t.integer :age1y
+      t.integer :age2y
+      t.integer :age3y
+      t.integer :age4y
+      t.integer :age5y
+      t.integer :age6y
+      t.integer :age7y
+      t.integer :age8y
+      t.integer :age9y
+      t.integer :age0m
+      t.integer :age1m
+      t.integer :age2m
+      t.integer :age3m
+      t.integer :age4m
+      t.integer :age5m
+      t.integer :age6m
+      t.integer :age7m
+      t.integer :age8m
+      t.integer :age9m
   		
       t.timestamps
     end
